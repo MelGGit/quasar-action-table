1f12349cbed944ebc135849460a22fbc55309bb0
Mel Grosser <m.grosser@curasoft.de>
2022-02-02 12:35:08 +0100

    add different Language

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
+
+const { t, availableLocales, locale } = useI18n()
+const toggleLocales = () => {
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+
 </script>
 
 <template>
   <img alt="Vue logo" src="@/assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <q-avatar color="warning" text-color="white">J</q-avatar>
-  <Test />
+  <div>{{ t('intro.whats-your-name') }}</div>
+  <button @click="toggleLocales">{{ t('button.toggle_langs') }}</button>
 </template>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Pages from "vite-plugin-pages"
+import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import Layouts from 'vite-plugin-vue-layouts'
 import Components from 'unplugin-vue-components/vite'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
@@ -25,12 +26,17 @@ export default defineConfig({
       imports: [
         'vue',
         'vue-router',
-        // 'vue-i18n',
+        'vue-i18n',
         // '@vueuse/head',
         // '@vueuse/core',
         // 'vitest',
       ],
       dts: 'src/auto-imports.d.ts',
+    }),
+      VueI18n({
+      runtimeOnly: true,
+      compositionOnly: true,
+      include: [resolve(__dirname, './src/locales/**')],
     }),
   ],
   resolve: {
