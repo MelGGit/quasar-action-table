5c81f2acc068a89992b9ae7a3320775cb35d4a78
Mel Grosser <m.grosser@curasoft.de>
2022-02-09 15:25:12 +0100

    initial button

---
+++ src/components/ButtonGroup.vue
@@ -0,0 +1,63 @@
+<script setup lang='ts'>
+const props = defineProps<{
+  toggleValue: String
+}>()
+const emit = defineEmits<{
+  (event: 'update:toggleValue', value: String): void
+}>()
+
+const valueComputed = computed({
+  get: () => props.toggleValue,
+  set: (value: String) => emit('update:toggleValue', value)
+})
+
+</script>
+
+<template>
+  <div>
+    <q-btn-toggle
+      v-model="valueComputed"
+      rounded
+      push
+      stack
+      no-caps
+      text-color="black"
+      toggle-text-color="white"
+      toggle-color="warning"
+      :options="[
+        { value: 'read', slot: 'read' },
+        { value: 'change', slot: 'change' },
+        { value: 'put', slot: 'put' },
+        { value: 'move', slot: 'move' }
+      ]"
+    >
+      <template v-slot:read>
+        <div class="col no-wrap">
+          <q-icon center name="directions_boat" />
+          <div class="text-center">Lesen</div>
+        </div>
+      </template>
+
+      <template v-slot:change>
+        <div class="col no-wrap">
+          <q-icon center name="directions_car" />
+          <div class="text-center">Eingabe</div>
+        </div>
+      </template>
+
+      <template v-slot:put>
+        <div class="col no-wrap">
+          <q-icon center name="directions_railway" />
+          <div class="text-center">Setzen</div>
+        </div>
+      </template>
+
+      <template v-slot:move>
+        <div class="col no-wrap">
+          <q-icon center name="directions_railway" />
+          <div class="text-center">Verschieben</div>
+        </div>
+      </template>
+    </q-btn-toggle>
+  </div>
+</template>
--- src/components/InnerLayout.vue
+++ src/components/InnerLayout.vue
@@ -1,6 +1,10 @@
 <script setup lang='ts'>
+const toggleValue = ref('read')
+
 </script>
  
 <template>
-  <div class="tw-h-[75vh] tw-bg-white tw-rounded-xl tw-shadow-xl"></div>
+  <div class="tw-h-[75vh] tw-bg-white tw-rounded-xl tw-shadow-xl tw-relative">
+    <ButtonGroup v-model:toggleValue="toggleValue" class="tw-absolute tw-top-0 tw-left-1/2" />
+  </div>
 </template>
