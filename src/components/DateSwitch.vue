7e67f0bfd20cf39b064d388018c8f6cdce8653bb
Mel Grosser <m.grosser@curasoft.de>
2022-02-21 12:46:26 +0100

    add new date component

---
+++ src/components/DateSwitch.vue
@@ -0,0 +1,12 @@
+<script setup lang='ts'>
+</script>
+ 
+<template>
+  <div class="tw-flex tw-justify-center tw-items-center">
+    <q-btn flat dense round color="black" icon="chevron_left" />
+    <span class="tw-text-lg">
+      <slot></slot>
+    </span>
+    <q-btn flat dense round color="black" icon="chevron_right" />
+  </div>
+</template>
