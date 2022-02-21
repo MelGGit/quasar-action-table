b4617639ae9de273b9aa10b30fead8679cbafef6
Mel Grosser <m.grosser@curasoft.de>
2022-02-21 15:30:54 +0100

    select whole text

--- src/components/GridTable.vue
+++ src/components/GridTable.vue
@@ -21,15 +21,26 @@ function reduceFunction(accumulator: number, currentValue: number | string): num
   return Number(accumulator) + Number(currentValue)
 }
 
+const selectWholeText = (event: FocusEvent) => {
+  if (event.target && event.target instanceof HTMLInputElement)
+    event.target.select()
+}
+
 </script>
  
 <template>
   <div class="tw-grid tw-grid-cols-[max-content_max-content_repeat(32,_minmax(0,_1fr))] tw-text-center">
-    <div class="tw-px-2" v-for="headName in tableHead" :key="headName">{{ headName }}</div>
-    <div class="tw-px-2" v-for="value in morningDateRow" :key="value">{{ value }}</div>
-    <div class="tw-px-2" v-for="value in morningVisitRow" :key="value">{{ value }}</div>
-    <div class="tw-px-2" v-for="value in morningInfo" :key="value">{{ value }}</div>
-    <input class="tw-text-center" v-for="(value, i) in morningValues" :key="value" v-model.lazy="morningValues[i]" />
-    <div class="tw-px-2">{{ morningSum }}</div>
+    <div v-for="headName in tableHead" :key="headName">{{ headName }}</div>
+    <div v-for="value in morningDateRow" :key="value">{{ value }}</div>
+    <div v-for="value in morningVisitRow" :key="value">{{ value }}</div>
+    <div v-for="value in morningInfo" :key="value">{{ value }}</div>
+    <input
+      @focus="selectWholeText"
+      class="tw-text-center"
+      v-for="(value, i) in morningValues"
+      :key="value"
+      v-model.lazy="morningValues[i]"
+    />
+    <div>{{ morningSum }}</div>
   </div>
 </template>
